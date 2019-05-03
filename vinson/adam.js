3d9ad8f49409ebdce9d66e33b6382e6dc652a5f4
parkersalterego <parkersalterego92@gmail.com>
2019-05-02 20:49:24 -0700

    revert memes

--- vinson/adam.js
+++ vinson/adam.js
@@ -1,5 +1,1 @@
-document.addEventListener("mousemove", () => {
-    const img = document.createElement("img");
-    img.src = "https://media.giphy.com/media/moHRAoGsLBg88/giphy.gif";
-    document.body.appendChild(img);
-})
+console.log("adam");
