cc5db02387c27737bdb98ba290dc1972548508dd
Vinson Fernandez <geekwise.vinson.fernandez@gmail.com>
2019-05-02 20:38:16 -0700

    add js

---
+++ vinson/vinson.js
@@ -0,0 +1,7 @@
+const container = document.createElement('div');
+document.body.appendChild(container);
+container.style['background-color'] = 'aqua';
+container.style['border- = 'aqua';
+document.body.style.backgroundColor = 'green';
+container.innerText = 'hello';
+
