67e90c8917b8e93f46922ccbfeb288268af1a390
Andrew <45747989+TheThinkingThing@users.noreply.github.com>
2019-05-02 20:43:36 -0700

    Update Andrew.js
    add div, div color, div height, and inner text

--- vinson/Andrew.js
+++ vinson/Andrew.js
@@ -1,1 +1,6 @@
-
+const container1 = document.createElement('div');
+document.body.appendChild(container);
+container.style['background-color'] = 'blanchedAlmond';
+container.style['border-top'] = '2px black dashed';
+container.style['height'] = '30vh';
+container.innerText = 'This is Andrew\'s box.';
