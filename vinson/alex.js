31f5dc7fb716e6c978e5382a5d9af6640a3b6df9
unknown <KinguArekkusu@gmail.com>
2019-05-07 19:52:54 -0700

    added some stuff

--- vinson/alex.js
+++ vinson/alex.js
@@ -1,1 +1,6 @@
 console.log("O hya friends");
+const container = document.createElement('div');
+document.body.appendChild(container);
+container.style['background-color'] = 'pink';
+document.body.style.backgroundColor = 'green';
+container.innerText = 'hello';
