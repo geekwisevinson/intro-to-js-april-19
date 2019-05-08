0b87a09dabc0abb30d499a4da2f88e9d347394a6
unknown <KinguArekkusu@gmail.com>
2019-05-07 20:13:43 -0700

    added some stuff

--- vinson/alex.js
+++ vinson/alex.js
@@ -1,6 +1,7 @@
-console.log("O hya friends");
+console.log("I wanted to buy a coffee but then I realized that I ran out of cache");
 const container = document.createElement('div');
 document.body.appendChild(container);
 container.style['background-color'] = 'pink';
 document.body.style.backgroundColor = 'green';
-container.innerText = 'hello';
+container.innerText = 'This is my js and you cant have it';
+container.style.border = '3px solid black';
