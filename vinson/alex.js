3d8ba364835da86ff143c93771d121e1b35eceab
unknown <KinguArekkusu@gmail.com>
2019-05-07 20:16:13 -0700

    added some stuff

--- vinson/alex.js
+++ vinson/alex.js
@@ -1,7 +1,9 @@
-console.log("I wanted to buy a coffee but then I realized that I ran out of cache");
-const container = document.createElement('div');
-document.body.appendChild(container);
-container.style['background-color'] = 'pink';
-document.body.style.backgroundColor = 'green';
-container.innerText = 'This is my js and you cant have it';
-container.style.border = '3px solid black';
+(function () {
+    console.log("I wanted to buy a coffee but then I realized that I ran out of cache");
+    const container = document.createElement('div');
+    document.body.appendChild(container);
+    container.style['background-color'] = 'pink';
+    document.body.style.backgroundColor = 'green';
+    container.innerText = 'This is my js and you cant have it';
+    container.style.border = '3px solid black';
+})();
