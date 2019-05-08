97c62ed3b4ce636234d348282e65f205252da221
geekwisevinson <geekwise.vinson.fernandez@gmail.com>
2019-05-07 20:14:37 -0700

    update iife

--- vinson/jose.js
+++ vinson/jose.js
@@ -1,6 +1,8 @@
-const container = document.createElement('div');
-document.body.appendChild(container);
-container.style['background-color'] = 'aqua';
-document.body.style.backgroundColor = 'green';
-container.innerText = 'hello boys and girls';
+(function(){
+    const container = document.createElement('div');
+    document.body.appendChild(container);
+    container.style['background-color'] = 'aqua';
+    document.body.style.backgroundColor = 'green';
+    container.innerText = 'hello boys and girls';
+})();
 
