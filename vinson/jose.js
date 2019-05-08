9130813f652b7e93bdfc4b61354d68792655e18d
Jose Barajas <barajas.jose452yahoo.com>
2019-05-07 19:52:35 -0700

    added code to jose.js

--- vinson/jose.js
+++ vinson/jose.js
@@ -1,1 +1,6 @@
-console.log("test");
+const joseContainer = document.createElement('div');
+document.body.appendChild(joseContainer);
+joseContainer.style['background-color'] = 'aqua';
+document.body.style.backgroundColor = 'green';
+joseContainer.innerText = 'hello boys and girls';
+
